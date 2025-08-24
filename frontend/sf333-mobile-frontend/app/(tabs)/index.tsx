aff62374c6449b340cb3f051794d2f8acda43c11
Kong <90138804+ItsKong@users.noreply.github.com>
2025-08-24 21:30:35 +0700

    Init/frontend

---
+++ frontend/sf333-mobile-frontend/app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet} from "react-native";
+
+export default function Index() {
+  return (
+    <View style={style.view}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  view: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+  },
+})
