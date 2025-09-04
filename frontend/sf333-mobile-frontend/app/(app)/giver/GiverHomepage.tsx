17af2c2a33597e0bd5b0f8b543838c8e05c48153
Kong <90138804+ItsKong@users.noreply.github.com>
2025-09-04 13:51:04 +0700

    working navigation page

---
+++ frontend/sf333-mobile-frontend/app/(app)/giver/GiverHomepage.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function GiverHomepage() {
+    return(
+        <View>
+            <Text>This is Giver homepage</Text>
+        </View>
+    )
+}
---
+++ frontend/sf333-mobile-frontend/app/(app)/taker/TakerHomepage.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TakerHomepages() {
+    return (
+        <View>
+            <Text>This is taker page</Text>
+        </View>
+    )
+}
